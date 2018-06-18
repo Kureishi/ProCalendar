f55170e4459de7d0e6389f02eb7a81b87a14edf1
Kureishi <kureishi.shivanand587@gmail.com>
2018-06-17 21:11:33 -0400

    -modified screen 3. didnt link to other activities yet

--- app/src/main/java/com/sachin/procalendar/studentActivity.java
+++ app/src/main/java/com/sachin/procalendar/studentActivity.java
@@ -1,14 +1,20 @@
 package com.sachin.procalendar;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class studentActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    EditText commuteTime;
+    TimePickerDialog timePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +23,29 @@ public class studentActivity extends AppCompatActivity implements AdapterView.On
 
         Spinner eduLevelSpinner = findViewById(R.id.educationLevelDropDown);
 
+        commuteTime = (EditText)findViewById(R.id.commuteTime);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,           // reference spinner/drop-down with array created
                 R.array.student_eduLevel, android.R.layout.simple_spinner_item);                    // layout file for single item
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);             // layout for drop-down item
         eduLevelSpinner.setAdapter(adapter);
 
         eduLevelSpinner.setOnItemSelectedListener(this);
+
+        commuteTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {                                                        // when commuteTime clicked
+
+                timePickerDialog = new TimePickerDialog(studentActivity.this, new TimePickerDialog.OnTimeSetListener() {   // initialize TimerPicker Dialog
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {      // allows user to choose time
+
+                        commuteTime.setText(hourOfDay + " hours " + minutes + " minutes");          // how display in EditText after
+                    }
+                }, 0, 0, false);
+                timePickerDialog.show();
+            }
+        });
     }
 
     @Override
