74e3b1c06176432011f8ee45af6a6884b934a5f5
Kureishi <kureishi.shivanand587@gmail.com>
2018-07-11 20:59:38 -0400

    -link quick setup XML and class files

--- app/src/main/java/com/sachin/procalendar/quickSetupPopupActivity.java
+++ app/src/main/java/com/sachin/procalendar/quickSetupPopupActivity.java
@@ -1,4 +1,13 @@
 package com.sachin.procalendar;
 
-public class quickSetupPopupActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class quickSetupPopupActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_setup_popup);
+    }
 }
